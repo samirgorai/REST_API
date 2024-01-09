354236f53d6b30e9391a8c5e090e338850579d1e
Samirgorai <samirgorai17@gmail.com>
2024-01-09 21:32:43 +0530

    commiting to be on safer side
    Signed-off-by: Samir Gorai <samirgorai17@gmail.com>

--- DRF_API_learn/User_app/forms.py
+++ DRF_API_learn/User_app/forms.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import User
-#from django import forms
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class Create_user_form(UserCreationForm):
     class Meta:
         model=User
         fields=['first_name','last_name','username','email']
+
+
+class Login_form(forms.Form):
+    username=forms.CharField(max_length=10)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
--- DRF_API_learn/User_app/views.py
+++ DRF_API_learn/User_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from User_app.forms import Create_user_form
+from User_app.forms import Create_user_form,Login_form
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 #home 
@@ -13,12 +15,12 @@ def create_user(request):
     Flag=False
     message=''
     user_form=Create_user_form()
-    print('---------inside create_user----------')
+    #print('---------inside create_user----------')
     if(request.method=="POST"):
-        print('---------inside create_user2----------')
+        #print('---------inside create_user2----------')
         user_form=Create_user_form(request.POST)
         if(user_form.is_valid()):
-            print('---------inside create_user3----------')
+            #print('---------inside create_user3----------')
             #save user credentials           
             try:
                 
@@ -27,12 +29,12 @@ def create_user(request):
                 email=request.POST.get('email')
                 username=request.POST.get('username')
                 password=request.POST.get('password')
-                print('----------')
-                print('first_name',first_name)
+                #print('----------')
+                print('first_name',first_name,"password:",password)
                 User.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
 
                 Flag=True
-                print('---------inside create_user4----------')
+                #print('---------inside create_user4----------')
                 #User.first_name,User.last_name,User.username,User.email,User.password]           
                 return render(request,'User_app/createusersuccess.html',{'username':username,'fname':first_name,'Lname':last_name})
             except:
@@ -47,4 +49,29 @@ def create_user(request):
 
     
 def user_login(request):
-    return render(request,'User_app/User_login.html')
+    
+    if(request.method=="POST"):
+        print('----------userlogin1-------')
+        login_form=Login_form(request.POST)
+        print("------login_form.is_valid()--------",login_form.is_valid(),"------------------")
+
+        username = request.POST['username']
+        password = request.POST['password']
+        print("--------username:",username,"-----password-----",password)
+        #user=authenticate(username=request.POST.get('username'),password=request.POST.get('password'))
+        user=authenticate(request,username=username,password=password)
+        print("----user-------",user,"-------------")
+        if user is not None:
+            login(request, user)
+            return render(request, 'User_app/api_token_page.html', {'username': request.POST.get('username')})
+            print('------ login success ---------')
+        else:
+            # Authentication failed
+            return render(request, 'User_app/User_login.html', {'form': login_form})
+            print('----------userlogin3-------')
+    else:
+        login_form=Login_form()                
+
+    return render(request,'User_app/User_login.html',{"form":login_form})
+
+
