571e9a3e0a4e2746fb9bb19ad6f2a3d0eb1ab240
Samirgorai <samirgorai17@gmail.com>
2024-01-12 09:19:22 +0530

    Added Static files and updated settings
    Signed-off-by: Samir Gorai <samirgorai17@gmail.com>

--- DRF_API_learn/DRF_API_learn/settings.py
+++ DRF_API_learn/DRF_API_learn/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPlATES_DIR=Path.joinpath(BASE_DIR,'templates')
-
+STATIC_DIR=Path.joinpath(BASE_DIR,'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -144,6 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+   
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
